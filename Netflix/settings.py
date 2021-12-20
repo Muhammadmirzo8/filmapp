204c29d9722ee75c20525ef75a93c5256fef8f23
Muhammadmirzo <95030401+Muhammadmirzo8@users.noreply.github.com>
2021-12-20 10:14:01 +0500

    Update settings.py

--- Netflix/settings.py
+++ Netflix/settings.py
@@ -123,7 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = "/static/"
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 django_heroku.settings(locals())
